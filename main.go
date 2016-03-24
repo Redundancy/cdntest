e0a3315b6f9afbd3494f3f451475783bd5445c3b
Daniel Speed <dan@the-nexus.co.uk>
2016-03-24 12:45:21 +0000

    Timeouts, some flags for testing sites that don't have DNS set locally.

--- main.go
+++ main.go
@@ -27,64 +27,154 @@ import (
 	"net/url"
 	"os"
 	"time"
+
+	"github.com/codegangsta/cli"
 )
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("cdntest must be called with a url")
-		os.Exit(1)
+var transport = &http.Transport{
+	Proxy: http.ProxyFromEnvironment,
+	ResponseHeaderTimeout: time.Second * 10,
+}
+
+// Use a custom http client to ensure sensible timeouts
+var DefaultClient = &http.Client{
+	Timeout:   10 * time.Second,
+	Transport: transport,
+}
+
+func getPort(scheme string) (int, error) {
+	switch scheme {
+	case "http":
+		return 80, nil
+	case "https":
+		return 443, nil
+	default:
+		return 0, fmt.Errorf("Expected http or https: \"%v\"", scheme)
+	}
+}
+
+func getMyPublicIP() (string, error) {
+	response, err := DefaultClient.Get("https://api.ipify.org")
+
+	if err != nil {
+		return "", fmt.Errorf(
+			"Could not contact https://api.ipify.org to get your public IP: %v",
+			err,
+		)
 	}
 
-	urlArg := os.Args[1]
+	defer response.Body.Close()
 
-	cdnURL, err := url.Parse(urlArg)
+	b, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return "", fmt.Errorf(
+			"Could not contact https://api.ipify.org to get your public IP: %v",
+			err,
+		)
+	}
+
+	return string(b), nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Author = "Daniel Robert Speed"
+	app.Usage = `
+	Used for testing HTTP(S) endpoints for their SSL certificates and performance.
+
+	See: https://github.com/Redundancy/cdntest
+	`
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "ip",
+			Usage: "Use an explicit IP address rather than DNS provided",
+		},
+		cli.StringFlag{
+			Name:  "cname",
+			Usage: "Use an alternative DNS entry to lookup the IP",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		if c.NArg() != 1 {
+			return
+		}
+
+		testHTTP(options{
+			url:   c.Args()[0],
+			ip:    c.String("ip"),
+			cname: c.String("cname"),
+		})
+	}
+
+	app.Run(os.Args)
+}
+
+type options struct {
+	url   string
+	ip    string
+	cname string
+}
+
+func testHTTP(o options) {
+	cdnURL, err := url.Parse(o.url)
+
 	if err != nil {
 		fmt.Printf("Could not parse url: \"%v\" - %v\n", cdnURL, err)
 		os.Exit(1)
 	}
 
-	port := 80
-	switch cdnURL.Scheme {
-	case "http":
-	case "https":
-		port = 443
-	default:
-		fmt.Printf("Expected http or https url: \"%v\"\n", urlArg)
+	port, err := getPort(cdnURL.Scheme)
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	fmt.Println("Running CDN test at", time.Now())
+	myPublicIP, err := getMyPublicIP()
 
-	if r, getErr := http.Get("https://api.ipify.org"); getErr == nil {
-		defer r.Body.Close()
-		if b, e := ioutil.ReadAll(r.Body); e == nil {
-			fmt.Println("Your public IP is", string(b))
-		}
-	} else {
-		fmt.Println("Could not contact https://api.ipify.org to get your public IP")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	fmt.Println("My public IP is:", myPublicIP)
 
 	var addrs []string
 
+	host := cdnURL.Host
+	if o.cname != "" {
+		host = o.cname
+	}
+
 	beforeLookup := time.Now()
-	addrs, err = net.LookupHost(cdnURL.Host)
-	fmt.Printf("Host lookup of %v took %v\n", cdnURL.Host, time.Now().Sub(beforeLookup))
+	addrs, err = net.LookupHost(host)
+	fmt.Printf("Host lookup of %v took %v\n", host, time.Now().Sub(beforeLookup))
+
 	if err != nil {
 		fmt.Println("Could not look up host:", err)
+		os.Exit(1)
 	}
+
+	fmt.Println("DNS returned:", addrs)
 
 	/*
 		resolver-identity.cloudfront.net reflects back the IP of the dns server that
 		looked it up.
 	*/
 	resolverIdentities, _ := net.LookupHost("resolver-identity.cloudfront.net")
-	resolverNames, _ := net.LookupAddr(resolverIdentities[0])
+	if len(resolverIdentities) > 0 {
+		resolverNames, _ := net.LookupAddr(resolverIdentities[0])
 
-	fmt.Printf(
-		"Your DNS resolver is %v (%v)\n",
-		resolverNames[0],
-		resolverIdentities[0],
-	)
+		fmt.Printf(
+			"Your DNS resolver is %v (%v)\n",
+			resolverNames[0],
+			resolverIdentities[0],
+		)
+	} else {
+		fmt.Printf("Could not identify your DNS resolver\n")
+	}
 
 	request, _ := http.NewRequest("GET", cdnURL.String(), nil)
 
@@ -94,11 +184,24 @@ func main() {
 	var conn net.Conn
 	var dialErr error
 
+	ip := addrs[0]
+	if o.ip != "" {
+		ip = o.ip
+	}
+
 	beforeDialTime := time.Now()
-	conn, dialErr = net.Dial("tcp", fmt.Sprintf("%v:%v", addrs[0], port))
+	conn, dialErr = net.DialTimeout(
+		"tcp",
+		fmt.Sprintf("%v:%v", ip, port),
+		10*time.Second,
+	)
 	afterDialTime := time.Now()
 
-	defer conn.Close()
+	defer func() {
+		if conn != nil {
+			conn.Close()
+		}
+	}()
 
 	if dialErr != nil {
 		fmt.Println("Could not contact server:", dialErr)
@@ -119,6 +222,7 @@ func main() {
 
 		beforeHandshakeTime := time.Now()
 		tlsCon := tls.Client(conn, config)
+		tlsCon.SetDeadline(time.Now().Add(10 * time.Second))
 		if hsErr := tlsCon.Handshake(); hsErr != nil {
 			fmt.Println("SSL handshake error:", hsErr)
 		}
@@ -135,7 +239,23 @@ func main() {
 
 	request.WriteProxy(conn)
 	beforeRead := time.Now()
-	response, _ := http.ReadResponse(bufio.NewReader(conn), request)
+	response, err := http.ReadResponse(bufio.NewReader(conn), request)
+
+	if err != nil || response == nil {
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			fmt.Println(
+				"Timeout reading response from",
+				cdnURL.String(),
+			)
+		} else {
+			fmt.Println(
+				"Did not get a response (after establishing connection),",
+				"server behind loadbalancer / CDN may be down:",
+				err,
+			)
+		}
+		os.Exit(1)
+	}
 
 	if response.Body != nil {
 		defer response.Body.Close()
@@ -148,7 +268,7 @@ func main() {
 
 		fmt.Printf(
 			"Reading %v bytes took %v (%v)\n",
-			response.Header.Get("content-length"),
+			len(b), //response.Header.Get("content-length"),
 			timeTaken,
 			bps,
 		)
